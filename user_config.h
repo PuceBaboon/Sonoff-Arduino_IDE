2d839b6b8dec9d84d2850cc543cdd05eac55653c
PuceBaboon <PuceBaboon@users.noreply.github.com>
2016-04-06 14:13:56 +0900

    Changed formatting slightly.    Added more comments.

--- user_config.h
+++ user_config.h
@@ -1,34 +1,44 @@
 /*
- * $Id: user_config.h,v 1.1 2016/04/04 14:45:06 anoncvs Exp $
+ * $Id: user_config.h,v 1.2 2016/04/06 05:12:13 anoncvs Exp $
  *
  * Sonoff Arduino implementation by Theo Arends
  */
+
+/* 
+ * PROJECT is used as the default topic delimiter. The group topic
+ * delimiter is this word + "s".
+ */
 #define PROJECT             "sonoff"
+
 #define VERSION             "1.0.5"
-#define CFG_HOLDER          0x20160309   // Change this value to load default configurations
+#define CFG_HOLDER          0x20160309	// Change this value to load default configurations.
+
 
 // Wifi
 #define STA_SSID            "YOUR-SSID-HERE"
 #define STA_PASS            "YOUR-PASSWD-HERE"
 #define WIFI_HOSTNAME       "ESP-%06X-%s"
 
+
 // Ota
 #define OTA_URL             "http://YOUR-HOST.AND.DOMAIN:80/api/arduino/"PROJECT".cpp.bin"
+
 
 // MQTT
 #define MQTT_HOST           "YOUR-MQTT-HOST.AND.DOMAIN"
 #define MQTT_PORT           1883
 
-#define MQTT_CLIENT_ID      "DVES_%06X"  // Also fall back topic using Chip Id = last 6 characters of MAC address
+#define MQTT_CLIENT_ID      "DVES_%06X"	// Also fall back topic using Chip Id = last 6 characters of MAC address.
 #define MQTT_USER           "DVES_USER"
 #define MQTT_PASS           "DVES_PASS"
 
-#define SUB_PREFIX          "cmnd"
-#define PUB_PREFIX          "stat"
-#define MQTT_GRPTOPIC       PROJECT"s"   // Group topic
+#define SUB_PREFIX          "cmnd"	// Sonoff devices subscribe to:- cmnd/MQTT_TOPIC and cmnd/MQTT_GRPTOPIC.
+#define PUB_PREFIX          "stat"	// They publish to:- stat/MQTT_TOPIC.
 #define MQTT_TOPIC          PROJECT
+#define MQTT_GRPTOPIC       PROJECT"s"	// Group topic.
+
 
 // Application
 #define MQTT_SUBTOPIC       "POWER"
-#define APP_TIMEZONE        9            // +9 hours (Tokyo)
-#define APP_POWER           0            // Saved power state Off
+#define APP_TIMEZONE        9		// +9 hours (Tokyo).
+#define APP_POWER           0		// Saved power state Off.
